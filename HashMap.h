b4b4ab6220dc0cc8e6e797a257c3bd3c1778ccad
anballa <ananya.balla@gmail.com>
2025-11-03 22:05:09 -0500

    updated hashmap header file

--- HashMap.h
+++ HashMap.h
@@ -5,4 +5,26 @@
 #ifndef HASHMAP_H
 #define HASHMAP_H
 
-#endif //HASHMAP_H
+#include "Record.h"
+#include <vector>
+#include <list>
+#include <string>
+using namespace std;
+
+class HashMap {
+private:
+    vector<list<pair<string, Record>>> table;
+    int capacity;
+    int hashFunc(const string &key);
+
+public:
+    HashMap(int size);
+    void insert(const string &key, const Record &record);
+    Record* search(const string &key);
+    void remove(const string &key);
+    void display();
+    std::vector<std::string> getAllKeys() const;
+
+};
+
+#endif
